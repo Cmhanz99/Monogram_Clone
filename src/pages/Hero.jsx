02b91258c745b13028c8473dd664b9a18965e200
Cmhanz99 <hanzhmagbal@gmail.com>
2025-06-15 19:41:23 +0800

    This is my commit

--- src/pages/Hero.jsx
+++ src/pages/Hero.jsx
@@ -10,8 +10,8 @@ const Hero = () => {
       <div className="absolute inset-0 bg-black/50"></div>
 
       <div className="relative z-10 text-white w-full h-full flex items-center justify-center flex-col">
-        <h2 className="text-4xl font-bold uppercase text-center
-        tracking-[.5rem] text-orange-200">A Console for <br /> Every Workflow</h2>
+        <h2 className="md:text-4xl text-3xl font-bold uppercase text-center
+        md:tracking-[.5rem] tracking-[0] text-orange-200">A Console for <br /> Every Workflow</h2>
         <p className="text-xl text-center mt-5 tracking-wide">Discover the perfect console for yours.</p>
 
         <p className="text-4xl absolute bottom-10 animate-bounce">
